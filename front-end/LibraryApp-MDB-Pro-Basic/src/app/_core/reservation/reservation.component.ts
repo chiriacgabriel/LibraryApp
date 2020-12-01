bb0b90b9f73f0db8f05bc9ee846f05f5362d1dad
Gabriel Chiriac <61273744+shantajj@users.noreply.github.com>
2020-12-02 00:40:53 +0200

    02/12/2020
    Reservation calendar draft

--- front-end/LibraryApp-MDB-Pro-Basic/src/app/_core/reservation/reservation.component.ts
+++ front-end/LibraryApp-MDB-Pro-Basic/src/app/_core/reservation/reservation.component.ts
@@ -1,14 +1,5 @@
 import {ChangeDetectionStrategy, Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
-import {
-  addDays,
-  addHours,
-  endOfDay,
-  endOfMonth,
-  isSameDay,
-  isSameMonth,
-  startOfDay,
-  subDays,
-} from 'date-fns';
+import {endOfDay, isSameDay, isSameMonth, parseISO, startOfDay, } from 'date-fns';
 import {Subject} from 'rxjs';
 import {
   CalendarEvent,
@@ -16,6 +7,8 @@ import {
   CalendarEventTimesChangedEvent,
   CalendarView,
 } from 'angular-calendar';
+import {ModalDirective} from 'ng-uikit-pro-standard';
+import {FormControl, FormGroup} from '@angular/forms';
 
 const colors: any = {
   red: {
@@ -40,6 +33,8 @@ const colors: any = {
 })
 export class ReservationComponent implements OnInit {
 
+  reservationForm: FormGroup;
+
   @ViewChild('modalContent', {static: true}) modalContent: TemplateRef<any>;
 
   view: CalendarView = CalendarView.Month;
@@ -54,13 +49,13 @@ export class ReservationComponent implements OnInit {
   };
 
   actions: CalendarEventAction[] = [
-    {
-      label: '<i class="fas fa-fw fa-pencil-alt"></i>',
-      a11yLabel: 'Edit',
-      onClick: ({event}: { event: CalendarEvent }): void => {
-        this.handleEvent('Edited', event);
-      },
-    },
+    // {
+    //   label: '<i class="fas fa-fw fa-pencil-alt"></i>',
+    //   a11yLabel: 'Edit',
+    //   onClick: ({event}: { event: CalendarEvent }): void => {
+    //     this.handleEvent('Edited', event);
+    //   },
+    // },
     {
       label: '<i class="fas fa-fw fa-trash-alt"></i>',
       a11yLabel: 'Delete',
@@ -74,52 +69,27 @@ export class ReservationComponent implements OnInit {
   refresh: Subject<any> = new Subject();
 
   events: CalendarEvent[] = [
-    {
-      start: subDays(startOfDay(new Date()), 1),
-      end: addDays(new Date(), 1),
-      title: 'A 3 day event',
-      color: colors.red,
-      actions: this.actions,
-      allDay: true,
-      resizable: {
-        beforeStart: true,
-        afterEnd: true,
-      },
-      draggable: true,
-    },
-    {
-      start: startOfDay(new Date()),
-      title: 'An event with no end date',
-      color: colors.yellow,
-      actions: this.actions,
-    },
-    {
-      start: subDays(endOfMonth(new Date()), 3),
-      end: addDays(endOfMonth(new Date()), 3),
-      title: 'A long event that spans 2 months',
-      color: colors.blue,
-      allDay: true,
-    },
-    {
-      start: addHours(startOfDay(new Date()), 2),
-      end: addHours(new Date(), 2),
-      title: 'A draggable and resizable event',
-      color: colors.yellow,
-      actions: this.actions,
-      resizable: {
-        beforeStart: true,
-        afterEnd: true,
-      },
-      draggable: true,
-    },
+    // {
+    //   start: subDays(startOfDay(new Date()), 1),
+    //   end: addDays(new Date(), 1),
+    //   title: 'A 3 day event',
+    //   color: colors.red,
+    //   actions: this.actions,
+    //   allDay: true,
+    //   resizable: {
+    //     beforeStart: true,
+    //     afterEnd: true,
+    //   },
+    //   draggable: true,
+    // },
   ];
 
-  activeDayIsOpen: boolean = true;
+  activeDayIsOpen = true;
 
   constructor() {
   }
 
-  dayClicked({date, events}: { date: Date; events: CalendarEvent[] }): void {
+  dayClicked({date, events}: { date: Date; events: CalendarEvent[] }, modalDirective: ModalDirective): void {
     if (isSameMonth(date, this.viewDate)) {
       if (
         (isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) ||
@@ -130,6 +100,10 @@ export class ReservationComponent implements OnInit {
         this.activeDayIsOpen = true;
       }
       this.viewDate = date;
+    }
+
+    if (events.length === 0) {
+      modalDirective.toggle();
     }
   }
 
@@ -156,13 +130,13 @@ export class ReservationComponent implements OnInit {
     // this.modal.open(this.modalContent, { size: 'lg' });
   }
 
-  addEvent(): void {
+  addEvent(modalDirective: ModalDirective): void {
     this.events = [
       ...this.events,
       {
-        title: 'New event',
-        start: startOfDay(new Date()),
-        end: endOfDay(new Date()),
+        title: this.reservationForm.value.clientList,
+        start: startOfDay(parseISO(this.reservationForm.value.startDate)),
+        end: endOfDay(parseISO(this.reservationForm.value.endDate)),
         color: colors.red,
         draggable: true,
         resizable: {
@@ -171,6 +145,8 @@ export class ReservationComponent implements OnInit {
         },
       },
     ];
+    modalDirective.toggle();
+    console.log(this.events);
   }
 
   deleteEvent(eventToDelete: CalendarEvent) {
@@ -186,6 +162,16 @@ export class ReservationComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.formReservation();
+  }
 
+  formReservation() {
+    this.reservationForm = new FormGroup({
+      bookList: new FormControl(''),
+      userList: new FormControl(''),
+      clientList: new FormControl(''),
+      startDate: new FormControl(''),
+      endDate: new FormControl('')
+    });
   }
 }
--- front-end/LibraryApp-MDB-Pro-Basic/src/app/app.module.ts
+++ front-end/LibraryApp-MDB-Pro-Basic/src/app/app.module.ts
@@ -6,7 +6,13 @@ import {HttpClientModule} from '@angular/common/http';
 import {AgmCoreModule} from '@agm/core';
 import {AppComponent} from './app.component';
 
-import {MDBBootstrapModulesPro, MDBSpinningPreloader, NavbarModule, StickyHeaderModule, ToastModule} from 'ng-uikit-pro-standard';
+import {
+  MDBBootstrapModulesPro,
+  MDBSpinningPreloader,
+  NavbarModule,
+  StickyHeaderModule,
+  ToastModule
+} from 'ng-uikit-pro-standard';
 import {HomeComponent} from './home/home.component';
 import {LoginComponent} from './login/login.component';
 import {ProfileComponent} from './profile/profile.component';
@@ -16,18 +22,18 @@ import {AppRoutingModule} from './app-routing.module';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {NotFoundComponent} from './_errors/not-found/not-found.component';
 import {UserComponent} from './user/user.component';
-import { AuthorComponent } from './_core/author/author.component';
-import { BookComponent } from './_core/book/book.component';
-import { MediaComponent } from './_core/media/media.component';
-import { AuthorDetailsComponent } from './_core/author/author-details/author-details.component';
-import { StatsCardComponent } from './dashboard/stats-card/stats-card.component';
-import { ClientComponent } from './_core/client/client.component';
-import { ReservationComponent } from './_core/reservation/reservation.component';
-import { ReservationHeaderComponent } from './_core/reservation/reservation-header/reservation-header.component';
-import { CalendarModule, DateAdapter } from 'angular-calendar';
-import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import {AuthorComponent} from './_core/author/author.component';
+import {BookComponent} from './_core/book/book.component';
+import {MediaComponent} from './_core/media/media.component';
+import {AuthorDetailsComponent} from './_core/author/author-details/author-details.component';
+import {StatsCardComponent} from './dashboard/stats-card/stats-card.component';
+import {ClientComponent} from './_core/client/client.component';
+import {ReservationComponent} from './_core/reservation/reservation.component';
+import {ReservationHeaderComponent} from './_core/reservation/reservation-header/reservation-header.component';
+import {CalendarModule, DateAdapter} from 'angular-calendar';
+import {adapterFactory} from 'angular-calendar/date-adapters/date-fns';
 import {CommonModule} from '@angular/common';
-import { FlatpickrModule } from 'angularx-flatpickr';
+import {FlatpickrModule} from 'angularx-flatpickr';
 
 @NgModule({
   declarations: [
